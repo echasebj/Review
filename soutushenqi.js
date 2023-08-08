94f6144fd7226422fda67e76595ffa3e33cc2be4
Reviewa <141471346+Reviewa@users.noreply.github.com>
2023-08-09 07:45:21 +0800

    Create soutushenqi.js

---
+++ soutushenqi.js
@@ -0,0 +1,30 @@
+/*************************************
+
+项目功能：搜图神器 解锁VIP(先登录)
+下载地址：https://t.cn/A6ogWd6z
+脚本作者：chxm1023
+使用方法：请先登录账号，再开脚本。
+
+**************************************
+
+[rewrite_local]
+^http:\/\/wallpaper\.soutushenqi\.com\/api\/.+\/account\/token url script-response-body https://raw.githubusercontent.com/Reviewa/Review/main/Rewrite/soutushenqi.js
+
+[mitm]
+hostname = wallpaper.soutushenqi.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.data.vipPastDueTime = 4092599350000;
+chxm1023.data.vipLabel = "高级用户";
+chxm1023.data.vipLabelLevel = 4;
+chxm1023.data.vipType = 1;
+chxm1023.data.pcVipType = 1;
+chxm1023.data.pcVipPastDueTime = 4092599349000;
+chxm1023.data.vitalityVipPastDueTime = 4092599349000;
+chxm1023.data.vitalityPcVipPastDueTime = 4092599349000;
+
+$done({body : JSON.stringify(chxm1023)});
