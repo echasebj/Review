31ff76b7f3e32680f644b6e6c21919bb7c191dd1
Reviewa <141471346+Reviewa@users.noreply.github.com>
2023-08-09 04:36:29 +0800

    Create Nicegrampro.js

---
+++ Nicegrampro.js
@@ -0,0 +1,20 @@
+/******************************
+
+脚本功能：Nicegram: Unlimited Text+解锁订阅
+下载地址：https://apps.apple.com/us/app/nicegram-ai-chat-for-telegram/id1608870673
+软件版本：1.3.7
+脚本作者：转载于彭于晏
+更新时间：2023-8-7
+使用声明：⚠️此脚本仅供学习与交流，请勿贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/Reviewa/Review/main/Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
+
+
+*******************************/
