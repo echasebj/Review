32f49179c491ded6f2426e03394fa9d4f48a9e01
Reviewa <141471346+Reviewa@users.noreply.github.com>
2023-08-09 09:52:50 +0800

    Update and rename kuwo.js to Kuwo.js

--- Kuwo.js
+++ Kuwo.js
@@ -9,7 +9,7 @@
 当前版本:10.4.6
 [rewrite_local]
 # 完美解锁酷我音乐
-^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/Reviewa/Review/main/kuwo.js
+^https?:\/\/.*\.(kuwo|lrts)\.(cn|me)\/(a\.p|music\.pay|(vip\/(v2|enc)\/(theme|user\/vip))|(EcomResource|(Mobile)?Ad)Serv(er|ice)).* url script-response-body https://raw.githubusercontent.com/Reviewa/Review/main/Kuwo.js
 [mitm] 
 hostname = *.kuwo.cn, *.lrts.me
 ********************************
