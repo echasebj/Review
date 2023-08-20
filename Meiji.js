e530649a08920fee47d45739cfd56b7e144d667b
Reviewa <141471346+Reviewa@users.noreply.github.com>
2023-08-20 20:02:20 +0800

    Create Meiji.js

---
+++ Meiji.js
@@ -0,0 +1,25 @@
+/*
+当前版本：2.21.1
+每记：https://apps.apple.com/app/id1572586388
+
+[rewrite_local]
+^https?:\/\/api\.wemore\.com\/mylogs\/users url script-response-body https://raw.githubusercontent.com/Reviewa/Review/main/Meiji.js
+
+[MITM]
+hostname = api.wemore.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.data.subscription = {
+      "isRenewable": true,
+      "subscriptionStatus": 1,
+      "membershipLevel": "PREMIUM",
+      "standardRemainDays": 666666,
+      "premiumExpireTime": 148204937166,
+      "expireAt": 148204937166,
+      "isInRedeem": true,
+      "isExistPaid": true,
+      "standardExpireTime": 6666666,
+      "nextTradeTime": 148204937166
+}
+$done({ body: JSON.stringify(guding) });
