2b0be47b05ddc1bce9ef31e99e4775e0bbc89e57
Reviewa <141471346+Reviewa@users.noreply.github.com>
2023-08-23 10:45:51 +0800

    Create Lanfan.js

---
+++ Lanfan.js
@@ -0,0 +1,16 @@
+/*
+当前版本:2.3.8
+懒饭：https://apps.apple.com/app/id1377082167
+
+[rewrite_local]
+^https?:\/\/lanfanapp\.com\/api\/v1 url script-response-body https://raw.githubusercontent.com/Reviewa/Review/main/Lanfan.js
+
+[MITM]
+hostname = lanfanapp.com
+
+*/
+var guding = $response.body;
+guding = guding.replace(/"is_purchased":\w+/g, '"is_purchased":true');
+guding = guding.replace(/"is_prime":\w+/g, '"is_prime":true');
+guding = guding.replace(/"unlocked":\w+/g, '"unlocked":true');
+$done({ body:guding});
